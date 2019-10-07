1c041deb8349b88663254a70cc8dd2d64a928ecc
Maxime Boisvert <1584942@cstjean.qc.ca>
2019-10-06 23:03:41 -0400

    Modal avec class

---
+++ wwwroot/js/BaseModal.js
@@ -0,0 +1,44 @@
+﻿class Modal {
+    constructor(modalId, modalClose, modalOpen, onOpenning, onClosing) {
+        this.modalId = modalId;
+        this.modalClose = modalClose;
+        this.modalOpen = modalOpen;
+        this.onOpenning = onOpenning;
+        this.onClosing = onClosing;
+
+        var modal = this;
+
+
+
+        $(this.modalOpen).each(function (index, btn) {
+            // When the user clicks the button, open the modal
+            $(btn).click(function () {
+                modal.openModal(this);
+            });
+        });
+
+        window.onclick = function (event) {
+            if (event.target == $("#myModal")[0]) {
+                modal.exitModal();
+            }
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        $(this.modalClose).each(function (index, btn) {
+            // When the user clicks the button, open the modal
+            $(btn).click(function () {
+                modal.exitModal(this);
+            });
+        });
+    }
+
+    openModal(sender) {
+        $("#myModal").css("display", "block");
+        this.onOpenning(sender);
+    }
+
+    exitModal() {
+        $("#myModal").css("display", "none");
+        this.onClosing();
+    }
+}
--- wwwroot/js/menuItemModal.js
+++ wwwroot/js/menuItemModal.js
@@ -1,35 +1,25 @@
-﻿// Get the modal
-var modal = $("#myModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-close")[0];
-
-// Get the button that opens the modal
-//var buttons = document.getElementsByClassName("modal-btn");
-$(".modal-btn").each(function (index, btn) {
-    // When the user clicks the button, open the modal
-    $(this).click(function () {
-        $("#myModal").css("display", "block");
+﻿class MenuItemModal extends Modal {
+    openFun = function () {
         $("#myModal").find("#modalPicture").attr("style", "background-image: url('data:image/png;base64," + $(this).children("data").attr("image") + "'); background-size: cover; background-position: center");
         $("#myModal").find("#itemId").attr("value", $(this).children("data").attr("itemId"));
         //$("#myModal").find("#modalContent").text($(this).children("data").attr("name"));
     }
-    );
-});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    exitModal();
-}
+    constructor() {
+        super('#myModal', '.modal-close', '.modal-btn',
+            function (sender) { this.openFun(sender) },
+            function (sender) { this.closeFun(sender) });
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == document.getElementById("myModal")) {
-        exitModal();
+    openFun = function (sender) {
+        $("#myModal").find("#modalPicture").attr("style", "background-image: url('data:image/png;base64," + $(sender).children("data").attr("image") + "'); background-size: cover; background-position: center");
+        $("#myModal").find("#itemId").attr("value", $(sender).children("data").attr("itemId"));
+        //$("#myModal").find("#modalContent").text($(this).children("data").attr("name"));
+    }
+
+    closeFun = function (sender) {
+        $("#quantity").val(1);
     }
 }
 
-function exitModal() {
-    $("#quantity").val(1);
-    $("#myModal").css("display", "none");
-}
+var modal = new MenuItemModal();
