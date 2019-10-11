829ae08987c82f6fa4f86e50548d815215cec46b
Maxime Boisvert <1584942@cstjean.qc.ca>
2019-10-11 18:07:57 -0400

    Ajout admin page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using technology_tp1.Models;
+using technology_tp1.Services;
+
+namespace technology_tp1.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View(this);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
