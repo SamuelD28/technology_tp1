f950c47590d1ce9212eeae2c8fff2f8b411adcaa
Maxime Boisvert <1584942@cstjean.qc.ca>
2019-10-11 18:50:47 -0400

    Ajour seed de la categorie

---
+++ Migrations/20191011225009_test.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace technology_tp1.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -4,
+                column: "Category",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -3,
+                column: "Category",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "Category",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -4,
+                column: "Category",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -3,
+                column: "Category",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "Category",
+                value: 0);
+        }
+    }
+}
